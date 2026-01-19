d187c801d5cddc0ef09f553839dc0fa623ae03b7
Jeevanantham <jeevaoff22@gmail.com>
2026-01-19 20:16:46 +0530

    ss

--- server.js
+++ server.js
@@ -1438,7 +1438,7 @@ app.use((req, res) => {
 /* =========================
    START SERVER
 ========================= */
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5003;
 
 
 app.listen(PORT, async () => {
